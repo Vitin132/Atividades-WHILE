fdd30d58157592b4b7a8cbe4b96a4dd9c9f317cb
Vitor Lopes <vitorlopes040@gmail.com>
2025-03-10 21:24:56 -0300

    correção

--- AT5_ParImpar.java
+++ AT5_ParImpar.java
@@ -48,27 +48,35 @@ public class AT5_ParImpar {
 
                 }
 
-                if (escolhaUser.equalsIgnoreCase("par") && soma % 2 == 0
-                        | escolhaUser.equalsIgnoreCase("ímpar") && soma % 2 != 0) {
+                if (escolhaUser.equalsIgnoreCase("par") && soma % 2 == 0) {
 
                     System.out.println("Você escolheu " + escolhaUser + " e colocou o número " + numUser + " e ganhou!");
                     System.out.println("O PC escolheu " + escolhaPC + " e colocou o número " + numPC + "!");
 
-                } else {
+                } else if(escolhaUser.equalsIgnoreCase("ímpar") && soma % 2 == 0) {
 
                     System.out.println("Você escolheu " + escolhaUser + " e colocou o número " + numUser + " e perdeu!");
                     System.out.println("O PC escolheu " + escolhaPC + " e colocou o número " + numPC + "!");
 
                 }
                 
-                if (escolhaUser.equalsIgnoreCase("sair")){
-                    
-                    System.out.println("Fim de jogo!");
+                if (escolhaUser.equalsIgnoreCase("par") && soma % 2 != 0) {
 
-                    
+                    System.out.println("Você escolheu " + escolhaUser + " e colocou o número " + numUser + " e perdeu!");
+                    System.out.println("O PC escolheu " + escolhaPC + " e colocou o número " + numPC + "!");
+
+                } else if(escolhaUser.equalsIgnoreCase("ímpar") && soma % 2 != 0) {
+
+                    System.out.println("Você escolheu " + escolhaUser + " e colocou o número " + numUser + " e ganhou!");
+                    System.out.println("O PC escolheu " + escolhaPC + " e colocou o número " + numPC + "!");
+
                 }
+                
+               
 
             }
+            
+            System.out.println("Fim de jogo!");
             
             
         }
